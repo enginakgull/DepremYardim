b28e23aafeba1b17799d201772e396c50d9e10a6
Gurcan-Unsal <gurcanunsal@outlook.com>
2023-05-12 17:19:55 +0300

    yardım et menüsü eklendi yardım al menüsü için button eklendi ama fonksiyonu yok

--- app/src/main/java/com/example/depremyardim/MainActivity.java
+++ app/src/main/java/com/example/depremyardim/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.depremyardim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button yardimEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // yorum deneme
+
+        yardimEt = (Button) findViewById(R.id.YardimEt);
+        yardimEt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                yardimEtMenu();
+            }
+        });
+    }
+
+    public void yardimEtMenu() {
+        Intent intent = new Intent(this, YardimEt.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/depremyardim/YardimEt.java
@@ -0,0 +1,14 @@
+package com.example.depremyardim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YardimEt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yardim_et);
+    }
+}
