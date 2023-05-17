d40104eaa08385e5cc49b6de3fab33bf5c391e87
Gurcan-Unsal <gurcanunsal@outlook.com>
2023-05-17 20:36:34 +0300

    bagis menü geçişi

---
+++ app/src/main/java/com/example/depremyardim/BagisEkrani.java
@@ -0,0 +1,14 @@
+package com.example.depremyardim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BagisEkrani extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bagis_ekrani);
+    }
+}
--- app/src/main/java/com/example/depremyardim/YardimEt.java
+++ app/src/main/java/com/example/depremyardim/YardimEt.java
@@ -2,13 +2,31 @@ package com.example.depremyardim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class YardimEt extends AppCompatActivity {
 
+    private Button ilerle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yardim_et);
+
+        ilerle = (Button) findViewById(R.id.bagisButton);
+
+        ilerle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bagisEkrani();
+            }
+        });
+    }
+
+    private void bagisEkrani() {
+        Intent intent = new Intent(this, BagisEkrani.class);
+        startActivity(intent);
     }
 }
