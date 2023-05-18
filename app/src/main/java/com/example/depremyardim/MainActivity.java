113f20aa55ec65b04c961a57e247ccbc1c592584
Engin AKGÜL <97546252+Egooneer@users.noreply.github.com>
2023-05-18 22:50:21 +0300

    yardim al kisimlarina ekleme yapildi.

--- app/src/main/java/com/example/depremyardim/MainActivity.java
+++ app/src/main/java/com/example/depremyardim/MainActivity.java
@@ -9,23 +9,41 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button yardimEt;
+    private Button yardimAl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        yardimEt = (Button) findViewById(R.id.YardimEt);
+        yardimEt = (Button) findViewById(R.id.button_yardim_et);
+        yardimAl = (Button) findViewById(R.id.button_yardim_al);
+
+        yardimAl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                yardimAlMenu();
+            }
+        });
+
+
         yardimEt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 yardimEtMenu();
             }
+
         });
+    }
+
+    public void yardimAlMenu(){
+        Intent intent = new Intent(this,yardimAl.class);
+        startActivity(intent);
     }
 
     public void yardimEtMenu() {
         Intent intent = new Intent(this, YardimEt.class);
         startActivity(intent);
     }
+
 }
