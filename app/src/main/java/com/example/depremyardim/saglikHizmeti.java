13bf63190a8758fb755e95cc3a9eb5ce4410df45
Engin AKGÜL <97546252+Egooneer@users.noreply.github.com>
2023-06-03 16:09:16 +0300

    yardim al kisimlarina ekleme yapildi.

--- app/src/main/java/com/example/depremyardim/saglikHizmeti.java
+++ app/src/main/java/com/example/depremyardim/saglikHizmeti.java
@@ -3,8 +3,16 @@ package com.example.depremyardim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class saglikHizmeti extends AppCompatActivity {
+
+    private Button goster;
+    private EditText sehir;
+    private TextView sonuc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +20,19 @@ public class saglikHizmeti extends AppCompatActivity {
         setContentView(R.layout.activity_saglik_hizmeti);
 
 
+
+        sehir = findViewById(R.id.sehirEditText);
+        goster = findViewById(R.id.goruntule);
+        sonuc = findViewById(R.id.sehirler);
+
+        goster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String il = sehir.getText().toString();
+                String snc = il + " Şehir Hastanesi ";
+                sonuc.setText(snc);
+            }
+        });
 
     }
 }
