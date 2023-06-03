2613a44303695131be9435368e82835f0a559500
Engin AKGÜL <97546252+Egooneer@users.noreply.github.com>
2023-06-03 16:50:49 +0300

    yardim al kisimlarina ekleme yapildi.

--- app/src/main/java/com/example/depremyardim/saglikHizmeti.java
+++ app/src/main/java/com/example/depremyardim/saglikHizmeti.java
@@ -2,11 +2,14 @@ package com.example.depremyardim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class saglikHizmeti extends AppCompatActivity {
 
@@ -29,8 +32,18 @@ public class saglikHizmeti extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String il = sehir.getText().toString();
-                String snc = il + " Şehir Hastanesi ";
-                sonuc.setText(snc);
+                String location = "https://www.google.com/maps/search/?api=1&query="+ il;
+                Uri gmmIntentUri = Uri.parse(location);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW,gmmIntentUri);
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    // Harita uygulaması bulunamadığı için bir hata mesajı gösterebilirsiniz
+                    Toast.makeText(saglikHizmeti.this, "Harita uygulaması bulunamadı. Tarayıcıda açılıyor...", Toast.LENGTH_SHORT).show();
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    startActivity(webIntent);
+                }
             }
         });
 
